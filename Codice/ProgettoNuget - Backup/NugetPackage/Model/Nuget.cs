fb98de505af94e6a034cfdd1ebc2952d43003993
Colugnat <alessandro.colugnat@samtrevano.ch>
2018-10-16 09:09:24 +0200

    no message

---
+++ Codice/ProgettoNuget - Backup/NugetPackage/Model/Nuget.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace NugetPackage.Model
+{
+    class Nuget
+    {
+        #region =================== costanti ===================
+        #endregion
+
+        #region =================== membri statici =============
+        #endregion
+
+        #region =================== membri & proprietà ===========
+        public string Directory { get; set; }
+        public string NamePackage { get; set; }
+        public string VersionPackage { get; set; }
+        public string VersionNewsPackage { get; set; }
+        public ObservableCollection<string> ResultSearch { get; set; }
+        public ObservableCollection<string> ResultSearchNews { get; set; }
+        public string StartSearch { get; set; }
+        public string ResultPackage { get; set; }
+        public string ResultLog { get; set; }
+        public string DescriptionPackage { get; set; }
+        public string DependencyPackage { get; set; }
+        #endregion
+
+        #region =================== costruttori ================
+        #endregion
+
+        #region =================== metodi aiuto ===============
+
+        #endregion
+
+        #region =================== metodi generali ============
+        #endregion
+    }
+}
---
+++ Codice/ProgettoNuget - Backup/NugetPackage/View/MainView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NuGet;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace NugetPackage.View
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void logArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            logArea.ScrollToEnd();
+        }
+    }
+}
